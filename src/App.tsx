bacf14dd3d4f0f39ea9d86b88d9346a8fb9cd9b3
Matheus Gomes <gomesprogramacao@gmail.com>
2021-11-15 14:45:31 -0300

    feat: useMeMo and when to using

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from "react";
 
 // components
-import { Item } from './components/item';
+import { Item } from "./components/item";
 
 function App() {
-  const [items, setItems] = useState<string[]>([])
+  const [items, setItems] = useState<string[]>([]);
+  const [newItem, setNewItem] = useState<string>("");
 
-// functions
-function addItemToList() {
-    setItems([...items , `Item ${items.length}`]);
-}
+  // functions
+  function addItemToList() {
+    setItems([...items, `Item ${items.length}`]);
+  }
+
+  const countItemsWithOne = useMemo(() => {
+    console.log("Render");
+    return items.filter((item) => item.includes("1")).length;
+  }, [items]);
 
   return (
-    <div className="App">
-      <button type="button" onClick={addItemToList}>Add</button>
+    <div style={{ display: "flex", flexDirection: "column", gap: "1rem 0" }}>
+      <input
+        type="text"
+        onChange={(event) => setNewItem(event.target.value)}
+        value={newItem}
+      />
+      <button type="button" onClick={addItemToList}>
+        Add
+      </button>
       <ul>
-        {items.map(item => {
-          return <Item key={item} title={item}/>
+        {items.map((item) => {
+          return <Item key={item} title={item} />;
         })}
       </ul>
+      <div>Count: {countItemsWithOne}</div>
     </div>
   );
 }
--- when-to-use.md
+++ when-to-use.md
@@ -4,3 +4,7 @@
 2. Renders too often
 3. Re-renders with same props
 4. Medium to big sizes
+
+# useMemo
+- When to use
+1. Avoid complex re-calculations 
